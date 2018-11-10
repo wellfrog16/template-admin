533e7d8ba41ce87f57a16aafb757fe2f08c90bea
wellfrog <wellfrog16@gmail.com>
2018-11-10 22:04:35 +0800

    细化stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,9 +7,10 @@ module.exports = {
     'selector-class-pattern': '^[a-z]+[a-z\\d/-]*[a-z\\d]$',  // class名字规则
     'selector-id-pattern': '^[a-zA-Z\\d]+$',  // id名字规则
     'max-nesting-depth': 3, // 最大嵌套深度
-    'max-empty-lines': 2,
+    'max-empty-lines': 1,
     'string-quotes': 'single',  // 单引号
     'function-url-quotes': 'always',  // 单引号
+    'at-rule-semicolon-newline-after': null,
     'property-no-unknown': [ true, {'ignoreProperties': ['return']} ],  // 自定义函数用，忽略return
     'selector-list-comma-newline-after': 'never-multi-line' // 不允许多行
   }
