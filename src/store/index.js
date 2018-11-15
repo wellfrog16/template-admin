7ee6b84801e1b5c852206075ee9ceb288d6157dc
wellfrog <wellfrog16@gmail.com>
2018-11-15 16:03:32 +0800

    完善vuex规范

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,30 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+// 模块
+import demo1 from '@/store/module/index/demo1';
+import hehe from '@/store/module/index/demo2/hehe';
+import haha from '@/store/module/index/demo2/haha';
+
 Vue.use(Vuex);
 
 const state = {
     count: 0
 };
 
-const mutations = {
-    updateCount(state, num) {
-        state.count += num || 1;
-    }
-};
+const mutations = {};
+const getters = {};
+const actions = {};
 
-// 创建 store 实例
+// 返回 store 实例
 export default new Vuex.Store({
     state,
-    mutations
+    mutations,
+    getters,
+    actions,
+    modules: {
+        demo1,
+        hehe,
+        haha
+    }
 });
